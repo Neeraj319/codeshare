b662fd4999f28930b4df80766dc6cc7e9bd8e369
Neeraj319 <itskop520@gmail.com>
2022-06-09 21:59:29 +0545

    updated manage.py [removed if else branching]

--- manage.py
+++ manage.py
@@ -10,23 +10,47 @@ def create_app(directory):
     )
 
 
-# try:
-args = sys.argv
-if len(args) == 2:
-    if sys.argv[1] == "create_tables":
-        run_async(create_tables())
-    if sys.argv[1] == "createsuperuser":
-        username = input('username: ')
-        password = input('password:')
-        run_async(create_super_user(username, password))
-    if sys.argv[1] == "createsuperuserauto":
-        run_async(create_super_user('123', '123'))
-elif len(args) == 3:
-    if sys.argv[1] == "createapp" and sys.argv[2]:
-        create_app(sys.argv[2])
-    else:
-        print("provide an app name")
+try:
+    first, main, *extra = sys.argv
+except ValueError:
+    pass
+
+
+def command_create_tables(*args):
+    run_async(create_tables())
+
+
+def createsuperuser(*args):
+    username = input('username: ')
+    password = input('password:')
+    run_async(create_super_user(username, password))
+
+
+def createsuperuserauto(*args):
+    run_async(create_super_user('123', '123'))
+
+
+commands = {
+    'create_tables': [command_create_tables, "creates table on the database"],
+    "createsuperuser": [createsuperuser, "creates a super user by asking username and password"],
+    "createsuperuserauto": [createsuperuserauto, "creates a superuser without asking username or password"],
+    "create_app": [create_app, "creates an app to the current working directory"],
+}
+
+
+def manage_help():
+    print(
+        """
+        Welcome to manage.py help down is the list of commands you can use 
+        """
+    )
+    for key, value in commands.items():
+        print(f"{key} | {value[1]}")
+
+
+commands['help'] = [manage_help, "shows all the commands available"]
+
+if values := commands.get(main):
+    values[0](*extra)
 else:
-    print("invalid arguments")
-# except Exception as e:
-#     print(e)
+    print('no such command found run "help" for more')
