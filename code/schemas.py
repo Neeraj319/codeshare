4147f1b8c6be8accf97a720f18af30d17a45e407
Neeraj319 <itskop520@gmail.com>
2022-06-10 14:22:32 +0545

    removed code app

--- code/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-
-class CodeSchema(BaseModel):
-    code: str
-    user: int
-    language: int
-    id: Optional[int] = None
--- code/urls.py
+++
@@ -1,5 +0,0 @@
-from fastapi.routing import APIRouter
-# from .views import create_code, create_language
-
-router = APIRouter(prefix="/code", tags=["code"])
-# router.post('/add/')(create_code)
