d0c0d93f93086b68ed156cd6360430ffb619eab2
Neeraj319 <itskop520@gmail.com>
2022-06-13 14:47:10 +0545

    added dev friendly docs to user_app

--- code_app/dependencies.py
+++ code_app/dependencies.py
@@ -1,45 +1,64 @@
-from tomlkit import value
 from code_app.schemas import CodeSchema
 from .models import Code
 from auth.models import User
 from language.models import Language
 import string
 import random
+from typing import Union
+from tortoise.queryset import QuerySet
 
 
-async def get_code_byid(id: int):
+async def get_code_byid(id: int) -> Union[Code, None]:
+    """
+    id -> int
+    returns code object or None
+    """
     return Code.get_or_none(id=id)
 
 
-async def generate_url():
+async def generate_slug():
+    """
+    function to generate unique slug for the code
+    """
     chars = string.ascii_letters
+    # generating random string
     url = "".join(random.choice(chars) for _ in range(5))
     for code in await Code.all():
         if code.url == url:
-            generate_url()
+            generate_slug()  # calls itself unless it finds unique slug
             break
     return url
 
 
-async def add_code(code: CodeSchema, user: User, language_id: int):
+async def add_code(code: CodeSchema, user: User, language_id: int) -> Union[Code, None]:
+    """
+    this function adds code to the database\n
+    if language with the given id is not available then
+    returns None
+    """
     if language := await Language.get_or_none(id=language_id):
         return await Code.create(
-            user=user, language=language, url=await generate_url(), text=code.text
+            user=user, language=language, url=await generate_slug(), text=code.text
         )
     return language
 
 
-async def get_code_by_slug(slug: str):
+async def get_code_by_slug(slug: str) -> Union[Code, None]:
+    """
+    returns code from the database with the given slug\n
+    if not available returns None
+    """
     return await Code.get_or_none(url=slug)
 
 
-async def get_all_from_db():
-    return await Code.all()
-
-
 async def update_code(code: Code, request_data: CodeSchema):
+    """
+    updates a particular data from the database
+    code -> Code Model
+    request_data -> data sent during patch request
+    """
     if request_data.__dict__.get("id"):
-        del request_data.id
+        del request_data.id  # this is necessary cause you can't alert the primary key
     for key, value in request_data:
         if value is not None:
             if (item := getattr(code, key)) and item != value:
@@ -49,4 +68,14 @@ async def update_code(code: Code, request_data: CodeSchema):
 
 
 async def remove_code(code: Code):
+    """
+    removes particular code from the database
+    """
     await code.delete()
+
+
+async def get_all_from_db(user: User) -> QuerySet[Code]:
+    """
+    returns all the code of the particular user
+    """
+    return await Code.filter(user_id=user.id)
--- code_app/views.py
+++ code_app/views.py
@@ -1,17 +1,28 @@
 from fastapi import Depends, HTTPException
 from fastapi_pagination import paginate
+from auth.models import User
 from code_app import dependencies
 from auth import dependencies as auth_dependency
+from code_app.models import Code
 from .schemas import CodeSchema, CodeUpdateSchema
 from auth import schemas as auth_schemas
 from starlette import status
-from admin import dependencies as admin_dependency
 
 
 async def post_code(
     code: CodeSchema,
-    user: auth_schemas.PydanticUser = Depends(auth_dependency.get_user_from_token),
+    user: auth_schemas.UserSchema = Depends(auth_dependency.get_user_from_token),
 ):
+    """
+        adds the given code to the database\n
+        user must be authenticated to use this route
+
+    {
+      'text': 'string',\n
+      'language_id': int,
+    }
+    """
+    # check fro language with given id is necessary
     if created_code := await dependencies.add_code(
         code=code, user=user, language_id=code.language_id
     ):
@@ -23,6 +34,9 @@ async def post_code(
 
 
 async def get_code(slug: str):
+    """
+    returns code object with the given slug else 404 not found
+    """
     if code := await dependencies.get_code_by_slug(slug=slug):
         return code
     else:
@@ -32,21 +46,28 @@ async def get_code(slug: str):
 
 
 async def get_all_code(
-    user: auth_schemas.PydanticUser = Depends(admin_dependency.get_super_user),
+    user: auth_schemas.UserSchema = Depends(auth_dependency.get_user_from_token),
 ):
-    if not user:
-        raise HTTPException(
-            detail="you are not allowed to update this code",
-            status_code=status.HTTP_401_UNAUTHORIZED,
-        )
-    return paginate(await dependencies.get_all_from_db())
+    """
+    returns all the code objects related to the particular user
+    """
+    return paginate(await dependencies.get_all_from_db(user=user))
 
 
 async def patch_code(
     slug: str,
     code: CodeUpdateSchema,
-    user: auth_schemas.PydanticUser = Depends(auth_dependency.get_user_from_token),
+    user: auth_schemas.UserSchema = Depends(auth_dependency.get_user_from_token),
 ):
+    """
+        this route updates the particular code object according to the slug passed else 404
+        both the fields are optional too
+    {
+      "text": "string",
+      "language_id": int
+    }
+
+    """
     if code_from_db := await dependencies.get_code_by_slug(slug=slug):
 
         if code_from_db.user_id == user.id:
@@ -63,8 +84,12 @@ async def patch_code(
 
 async def delete_code(
     slug: str,
-    user: auth_schemas.PydanticUser = Depends(auth_dependency.get_user_from_token),
+    user: auth_schemas.UserSchema = Depends(auth_dependency.get_user_from_token),
 ):
+    """
+    deletes particular code using the slug from the database
+    else if not found 404
+    """
 
     if code_from_db := await dependencies.get_code_by_slug(slug=slug):
         if code_from_db.user_id == user.id:
